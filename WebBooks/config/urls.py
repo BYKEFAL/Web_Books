addcc1ac4fb8680db43bee3a12f61d8a76d47695
expert <oleg-expert@mail.ru>
2024-04-22 17:29:50 +0300

    change admin panel

--- WebBooks/config/urls.py
+++ WebBooks/config/urls.py
@@ -15,10 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from catalog import views
 
+# добавлено для показа картинок
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', include('catalog.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
